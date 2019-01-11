22d52f715a7d01a1fb1fd01dc2e3e64c3238895a
Agano oasis <oasisagano@gmail.com>
2019-01-11 23:57:58 +0900

    Basic start game scaffold

---
+++ app/src/main/java/com/example/oasis/braintrainer/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.oasis.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button goButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.goButton);
+    }
+
+    public void startGame(View view) {
+        goButton.setVisibility(View.INVISIBLE);
+    }
+}
