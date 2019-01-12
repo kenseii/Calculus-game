90969e7e623df0fffd00954425e73cead134770c
Agano oasis <oasisagano@gmail.com>
2019-01-12 16:07:44 +0900

    Random number generator

--- app/src/main/java/com/example/oasis/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/oasis/braintrainer/MainActivity.java
@@ -4,8 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    // initiate all the global variables
+
     Button goButton;
 
     @Override
@@ -14,9 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         goButton = findViewById(R.id.goButton);
+        TextView sumText = findViewById(R.id.SumText);
+
+        // random instance
+        Random rand = new Random();
+
+        // Numbers to add
+        int firstNumber = rand.nextInt(21);
+        int secondNumber = rand.nextInt(21);
+
+        sumText.setText(Integer.toString(firstNumber) + " + " + Integer.toString(secondNumber));
+
+
     }
 
     public void startGame(View view) {
+        // method to launch the game
         goButton.setVisibility(View.INVISIBLE);
+    }
+
+    // method to choose the answer from the given one
+
+    public void chooseAnswer(View view) {
+
     }
 }
