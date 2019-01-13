12ea7d9bd104fc80b056b0883e84dbd0eca0ec6b
Agano oasis <oasisagano@gmail.com>
2019-01-14 00:24:57 +0900

    Game restart feature and code restructure

--- app/src/main/java/com/example/oasis/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/oasis/braintrainer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.oasis.braintrainer;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
     Button button3;
+    Button playAgainButton;
+    ConstraintLayout gameLayout;
     // random instance
     Random rand = new Random();
 
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         goButton = findViewById(R.id.goButton);
+        goButton.setVisibility(View.VISIBLE);
 
         // answer buttons
         button0 = findViewById(R.id.button0);
@@ -48,13 +52,41 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
 
+        //play again button
+        playAgainButton = findViewById(R.id.playAgain);
+        gameLayout = findViewById(R.id.gameLayout);
+        gameLayout.setVisibility(View.INVISIBLE);
+
         sumText = findViewById(R.id.SumText);
         resultTextView = findViewById(R.id.resultTextView);
         scoreTextView = findViewById(R.id.marksText);
         timerTextView = findViewById(R.id.timerText);
 
+        newQuestion();
+
+
+
+
+    }
+
+
+    //method to reset the game
+    public void playAgain(View view) {
+        score = 0;
+        numberOfQuestions = 0;
+        // reset the text to 30s
+        timerTextView.setText("30s");
+        // reset the text view
+        scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+
 
         newQuestion();
+        button0.setEnabled(true);
+        button1.setEnabled(true);
+        button2.setEnabled(true);
+        button3.setEnabled(true);
+        playAgainButton.setVisibility(View.INVISIBLE);
+        resultTextView.setText("");
 
         // start countdown timer
 
@@ -66,13 +98,21 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                // reset the text to 30s
-                timerTextView.setText("30s");
+
                 // set the result text to done/finish the session
                 resultTextView.setText("Done!!!!");
+                // show the play again button
+                playAgainButton.setVisibility(View.VISIBLE);
+
+                // set grid unclickable
+
+                button0.setEnabled(false);
+                button1.setEnabled(false);
+                button2.setEnabled(false);
+                button3.setEnabled(false);
+
             }
         }.start();
-
 
     }
 
@@ -80,6 +120,10 @@ public class MainActivity extends AppCompatActivity {
     public void startGame(View view) {
         // method to launch the game
         goButton.setVisibility(View.INVISIBLE);
+        // the method expects a view that's why the timer text is passed in
+        playAgain(timerTextView);
+        //show game layout
+        gameLayout.setVisibility(View.VISIBLE);
     }
 
     // this method is returns a new question once it is called
